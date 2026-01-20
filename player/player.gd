59390ba113d8fa1ade0434dbfcbffb642ec9a27e
benwayland-cell <ben.wayland2007@gmail.com>
2026-01-20 14:46:26 -0700

    Added variable jump heights

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var fall_grav :int= 1000
 @export var speed :int= 10000
 @export var jump_velocity :int= 300
+@export var min_jump_velocity :int= 50
 
 enum PlayerState {ON_GROUND, JUMPING, FALLING}
 
@@ -21,9 +22,10 @@ func _process(delta: float) -> void:
 func _update_player_state() -> void:
 	if (is_on_floor()):
 		player_state = PlayerState.ON_GROUND
-	elif (velocity.y < 0):
-		player_state = PlayerState.JUMPING
-	else:
+	
+	# if the player has let go of the jump button and has reached the min_jump_velocity
+	# or they are falling
+	elif ((velocity.y < -min_jump_velocity and not Input.is_action_pressed("jump")) or velocity.y > 0):
 		player_state = PlayerState.FALLING
 
 func _handle_left_right(delta: float) -> void:
@@ -38,8 +40,10 @@ func _handle_left_right(delta: float) -> void:
 
 
 func _handle_jumping() -> void:
+	# handle an initial jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_velocity
+		player_state = PlayerState.JUMPING
 	
 
 func _handle_grav(delta: float) -> void:
