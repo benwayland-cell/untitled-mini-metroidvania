2a6a74545bf6f2b061a17915b555bbe4ed3a2658
benwayland-cell <ben.wayland2007@gmail.com>
2026-01-20 19:52:32 -0700

    Fully implemented a sword

--- enemies/test_enemy.gd
+++ enemies/test_enemy.gd
@@ -1,4 +1,13 @@
 extends StaticBody2D
 
+@export var health :int= 2
+
+signal died
+
 func take_damage(damage_amount: int) -> void:
 	print("Took " + str(damage_amount) + " damage")
+	health -= damage_amount
+	
+	if health <= 0:
+		died.emit()
+		queue_free()
--- player/player.gd
+++ player/player.gd
@@ -6,8 +6,7 @@ extends CharacterBody2D
 @export var jump_velocity :int= 300
 @export var min_jump_velocity :int= 50
 
-@export var sword_active_time :float= 0.5
-@export var sword_cooldown_time :float= 1.0
+@export var sword_cooldown_time :float= 0.1
 
 # abililities unlocked
 var has_sword :bool= false
@@ -16,10 +15,10 @@ var has_double_jump_ability :bool= false
 enum PlayerState {ON_GROUND, JUMPING, FALLING}
 
 var player_state := PlayerState.FALLING
+var left_right_direction :int= 0
 
-var sword_is_active :bool= false
+var sword_cooldown_done :bool= true
 var can_double_jump :bool= true
-
 
 func _process(delta: float) -> void:
 	_update_player_state()
@@ -28,8 +27,20 @@ func _process(delta: float) -> void:
 	_handle_jumping()
 	_handle_sword()
 	_handle_debug()
+	_handle_animation()
+	
+	%Sword.get_overlapping_bodies()
 	
 	move_and_slide()
+
+func _handle_animation() -> void:
+	# flip the sword's position depending on which way we're going
+	if (%Sword.visible): # don't change it when it's visible
+		return
+	if left_right_direction == -1:
+		%Sword.position = -abs(%Sword.position)
+	elif left_right_direction == 1:
+		%Sword.position = abs(%Sword.position)
 
 func _update_player_state() -> void:
 	if (is_on_floor()):
@@ -42,14 +53,14 @@ func _update_player_state() -> void:
 		player_state = PlayerState.FALLING
 
 func _handle_left_right(delta: float) -> void:
-	var direction :float= 0.0
+	left_right_direction = 0
 	
 	if Input.is_action_pressed("left"):
-		direction += -1.0
+		left_right_direction += -1
 	if Input.is_action_pressed("right"):
-		direction += 1.0
+		left_right_direction += 1
 	
-	velocity.x = direction * delta * speed
+	velocity.x = left_right_direction * delta * speed
 
 
 func _handle_jumping() -> void:
@@ -80,14 +91,22 @@ func _handle_grav(delta: float) -> void:
 
 
 func _handle_sword() -> void:
-	if not (Input.is_action_just_pressed("sword") and not sword_is_active):
+	if not (Input.is_action_just_pressed("attack") and has_sword and sword_cooldown_done):
 		return
 	
-	sword_is_active = true
+	#swing the sword
+	sword_cooldown_done = false
 	%Sword.show()
-	await get_tree().create_timer(sword_active_time).timeout
-	sword_is_active = false
+	for body in %Sword.get_overlapping_bodies():
+		if body.is_in_group("enemies"):
+			body.take_damage(1)
+	
+	# wait until the sword should be hidden
+	await get_tree().create_timer(0.1).timeout
 	%Sword.hide()
+	
+	await get_tree().create_timer(sword_cooldown_time).timeout
+	sword_cooldown_done = true
 
 
 func _handle_debug() -> void:
@@ -97,11 +116,3 @@ func _handle_debug() -> void:
 	if Input.is_action_just_pressed("debug 2"):
 		has_double_jump_ability = !has_double_jump_ability
 		print("Double jump: " + str(has_double_jump_ability))
-
-
-func _on_sword_body_entered(body: Node2D) -> void:
-	if not sword_is_active:
-		return
-	
-	if body.is_in_group("enemies"):
-		body.take_damage(1)
