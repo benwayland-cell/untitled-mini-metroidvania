74c664c227e5ff0d9bc57f133d31e985923873ba
benwayland-cell <ben.wayland2007@gmail.com>
2026-03-08 18:48:11 -0600

    Gave the basic slime enemy a simple sprite and animation.

--- enemies/basic_slime/basic_slime.gd
+++ enemies/basic_slime/basic_slime.gd
@@ -8,11 +8,16 @@ extends Enemy
 
 @export var jump_vector := Vector2(200, -200)
 
+@onready var sprite: Sprite2D = %Sprite2D
 @onready var jump_timer: Timer = %JumpTimer
 
 enum State {DEFAULT, APROACHING, JUMPING}
 var state: State:
 	set = _set_state
+
+const FACING_CENTER_FRAME: int = 1
+const FACING_LEFT_FRAME: int = 2
+const FACING_RIGHT_FRAME: int = 0
 
 
 func _ready() -> void:
@@ -29,9 +34,24 @@ func _process(delta: float) -> void:
 	super._process(delta)
 	
 	_state_process()
+	_handle_animation()
 	
 	move_and_slide()
 
+
+func _handle_animation() -> void:
+	# don't change anything if we can't see the player
+	# or we are in the middle of jumping
+	if state == State.JUMPING or not player_is_visible:
+		return
+	
+	if position.x < player.position.x:
+		sprite.frame = FACING_RIGHT_FRAME
+	else:
+		sprite.frame = FACING_LEFT_FRAME
+
+
+################    States
 
 func _set_state(new_state: State) -> void:
 	state = new_state
