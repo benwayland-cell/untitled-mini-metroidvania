9af5c558792530a0cfd48b103c5d5c21e5782918
benwayland-cell <ben.wayland2007@gmail.com>
2026-03-20 19:39:38 -0600

    Made it so fading is only done by the explosion scene and not each chunk. Should help performance

--- explosion_animation/explosion.gd
+++ explosion_animation/explosion.gd
@@ -6,19 +6,47 @@ extends Node2D
 @export var spacing: float = 4.0
 @export var offset := Vector2(0, 8.0)
 
+@export_category("Fading")
+@export var time_until_fade: float = 0.5
+@export var fade_speed: float = 1.0
+
+@onready var timer: Timer = Timer.new()
+
 const EXPLOSION_CHUNK_SCENE: PackedScene = preload("uid://da4khkeph1ri3")
+
+var fading: bool = false
 
 
 func _ready() -> void:
-	# get the pos of the top-left chunk
+	# init fading timer
+	timer.wait_time = time_until_fade
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+	timer.start()
+	
+	# get the pos of the bottom-left chunk
 	var starting_pos := Vector2.ZERO
 	starting_pos.x = (dimensions.x - 1) / 2.0 * spacing
 	starting_pos.y = (dimensions.y - 1) / 2.0 * spacing
 	starting_pos += offset
 	
+	# make the explosion chunks
 	for row_index in range(dimensions.y):
 		for col_index in range(dimensions.x):
 			var new_chunk: ExplosionChunk = EXPLOSION_CHUNK_SCENE.instantiate()
 			new_chunk.color = color
 			new_chunk.position = starting_pos + Vector2(col_index * spacing, row_index * spacing)
 			add_child(new_chunk)
+
+
+
+func _process(delta: float) -> void:
+	if fading:
+		modulate.a -= fade_speed * delta
+		
+		if modulate.a <= 0:
+			queue_free()
+
+
+func _on_timer_timeout() -> void:
+	fading = true
--- explosion_animation/explosion_chunk.gd
+++ explosion_animation/explosion_chunk.gd
@@ -4,25 +4,17 @@ extends RigidBody2D
 @export var speed: float = 300.0
 @export var spread: float = PI * 0.75
 
-@export var time_until_fade: float = 0.5
-@export var fade_speed: float = 1.0
-
 @onready var color_rect: ColorRect = %ColorRect
-@onready var timer: Timer = Timer.new()
 
 var color: Color
 
-var fading: bool = false
+
 
 
 func _ready() -> void:
 	color_rect.color = color
 	
-	# init fading timer
-	timer.wait_time = time_until_fade
-	timer.timeout.connect(_on_timer_timeout)
-	add_child(timer)
-	timer.start()
+	
 	
 	# set inital velocity
 	var angle = randf_range(-spread / 2, spread / 2) - PI / 2
@@ -30,15 +22,3 @@ func _ready() -> void:
 	var velocity = direction * randf_range(speed * 0.5, speed)
 	apply_central_impulse(velocity)
 	angular_velocity = randf_range(-8.0, 8.0)
-
-
-func _process(delta: float) -> void:
-	if fading:
-		modulate.a -= fade_speed * delta
-		
-		if modulate.a <= 0:
-			queue_free()
-
-
-func _on_timer_timeout() -> void:
-	fading = true
